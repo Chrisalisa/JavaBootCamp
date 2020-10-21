700e18fd9fe9e55894689fd238698c39774bfecc
Chrisalisa <kikistaurins@gmail.com>
2020-10-21 17:42:40 +0300

    End of day

---
+++ SEF/src/sef/module6/activity/Rectangle.java
@@ -0,0 +1,21 @@
+package sef.module6.activity;
+
+public class Rectangle extends Shape{
+	private double length;
+	private double width;
+
+	
+
+	@Override
+	public double calculateArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
